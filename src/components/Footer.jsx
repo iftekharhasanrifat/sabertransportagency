a94f6a3403c1c206c1389065931be36daf346461
iftekharhasanrifat <iftekharhasanrifat@gmail.com>
2025-03-01 13:25:36 +0600

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,9 +16,9 @@ const Footer = () => {
           {/* Contact Info */}
           <div>
             <h3 className="text-lg font-semibold">Contact Us</h3>
-            <p className="text-gray-400 mt-2">Email: info@sabertransport.com</p>
-            <p className="text-gray-400">Phone: +880 1234-567890</p>
-            <p className="text-gray-400">Address: Chittagong, Bangladesh</p>
+            <p className="text-gray-400 mt-2">Email: sabertraders059@gmail.com</p>
+            <p className="text-gray-400">Phone: +880 1676-384999</p>
+            <p className="text-gray-400">Address: Hazi Abdus salam market,Moijjertek, charpathor ghata,Karnaphuli, Chattogram</p>
           </div>
         </div>
 
